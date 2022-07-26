2f792fc6e93cb19ec3519cadf7c4f9dc2e14b585
jeong eun <koyy001@postech.ac.kr>
2022-07-27 06:04:17 +0900

    map

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from setting import *
 from player import player
+from tile import tile
 from balloon import balloon
 import copy
 import time
@@ -16,18 +17,82 @@ pygame.display.set_caption(title)
 # 3. 게임 내 필요한 설정
 clock = pygame.time.Clock()
 
+# sprite group setup
+visible_sprites = pygame.sprite.Group()
+obstacle_sprites = pygame.sprite.Group()
+
 moving_sprites = pygame.sprite.Group()
-player1 = player(screen,"asset/balloon/pink.png",0,0)
+over_sprites = pygame.sprite.Group()
+player1 = player((0,0),[visible_sprites],obstacle_sprites)
 moving_sprites.add(player1)
 
 balloon1 = balloon(screen, "asset/balloon/yellow.png")
 now_count = balloon1.balloonCount
 
+
+tile_img = []
+tile_img.append(pygame.image.load('asset/tile/land.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/tree_under.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/redBlock.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/orangeBlock.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/redHouse_under.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/box.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/blueHouse_under.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/road.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/tree_up.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/redHouse_up.png').convert_alpha())
+tile_img.append(pygame.image.load('asset/tile/blueHouse_up.png').convert_alpha())
+
+tile_img[0]= pygame.transform.scale(tile_img[0], (64, 64))
+tile_img[1]= pygame.transform.scale(tile_img[1], (64, 64))
+tile_img[2]= pygame.transform.scale(tile_img[2], (64, 64))
+tile_img[3]= pygame.transform.scale(tile_img[3], (64, 64))
+tile_img[4]= pygame.transform.scale(tile_img[4], (64, 64))
+tile_img[5]= pygame.transform.scale(tile_img[5], (64, 64))
+tile_img[6]= pygame.transform.scale(tile_img[6], (64, 64))
+tile_img[7]= pygame.transform.scale(tile_img[7], (64, 64))
+tile_img[8]= pygame.transform.scale(tile_img[8], (64, 108))
+tile_img[9]= pygame.transform.scale(tile_img[9], (64, 94))
+tile_img[10]= pygame.transform.scale(tile_img[10], (64, 94))
+
 m_list = []
 k = 0
 ball_released = 0
 
+# 타일 깔아주기
 
+for row_index,row in enumerate(WORLD_MAP):
+    for col_index, col in enumerate(row):
+        x = col_index * tilesize
+        y = row_index * tilesize
+        if col == 'l':
+            tile((x,y),[visible_sprites], "land", tile_img)
+        elif col == 'o':
+            tile((x,y),[visible_sprites,obstacle_sprites], "orangeBlock", tile_img)
+        elif col == 'r':
+            tile((x,y),[visible_sprites,obstacle_sprites], "redBlock", tile_img)
+        elif col == 'R':
+            tile((x,y),[visible_sprites,obstacle_sprites], "redHouse_under", tile_img)
+        elif col == 'b':
+            tile((x,y),[visible_sprites,obstacle_sprites], "box", tile_img)
+        elif col == 't':
+            tile((x,y),[visible_sprites,obstacle_sprites], "tree_under", tile_img)
+        elif col == 'B':
+            tile((x,y),[visible_sprites,obstacle_sprites], "blueHouse_under", tile_img)
+        elif col == 'g':
+            tile((x,y),[visible_sprites], "road", tile_img)
+
+for row_index,row in enumerate(WORLD_MAP):
+    for col_index, col in enumerate(row):
+        x = col_index * tilesize
+        y = row_index * tilesize
+        if col == 'R':
+            tile((x,y),[over_sprites], "redHouse_up",tile_img)
+        elif col == 't':
+            tile((x,y),[over_sprites], "tree_up",tile_img)
+        elif col == 'B':
+            tile((x,y),[over_sprites], "blueHouse_up",tile_img)
+            
 
 # 4. 메인 이벤트
 SB = 0
@@ -84,19 +149,27 @@ while SB == 0:
     if space_go == False:
         ball_released = 0
 
-    # 4-4. 그리기
-    screen.fill(white)
+    # 스크린 리셋하기
+    screen.fill(gray)
 
+    # 타일 그리기
+    visible_sprites.draw(screen)
+    
+    # 물풍선 그리기
     for m in m_list:
         m.showBalloon()
         if time.time() - m.initTime > 3:
             m_list.remove(m)
             now_count +=1
 	
-    # player1.show()
+    # player 그리기
     moving_sprites.draw(screen)
-    # moving_sprites.update()
+    
+    # 입체적 타일 그리기
+    over_sprites.draw(screen)
 
+    #visible_sprites.draw(screen)
+    print(over_sprites)
     # 4-5. 업데이트
     pygame.display.flip()
 
--- player.py
+++ player.py
@@ -4,11 +4,10 @@ from setting import *
 class player(pygame.sprite.Sprite):
 
     # 생성자
-    def __init__(self, screen, player_img, x, y):
-        super().__init__()
-        # 스크린
-        self.screen=screen
-
+    def __init__(self, pos, groups, obstacle_sprites):
+        super().__init__(groups)
+    
+        
         self.sprites = []
         self.sprites.append(pygame.image.load('asset/character/down_1.png').convert_alpha())
         self.sprites.append(pygame.image.load('asset/character/down_2.png').convert_alpha())
@@ -27,36 +26,38 @@ class player(pygame.sprite.Sprite):
         self.sprites.append(pygame.image.load('asset/character/right_3.png').convert_alpha())
         self.sprites.append(pygame.image.load('asset/character/right_4.png').convert_alpha())
         for i in range(len(self.sprites)):
-            self.sprites[i] = pygame.transform.scale(self.sprites[i], (100,100))
-
+            self.sprites[i] = pygame.transform.scale(self.sprites[i], (80,80))
 
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
-        self.rect = self.image.get_rect()
-        self.rect.topleft = [x,y]
-
-
+        #self.rect = self.image.get_rect()
+        #self.rect.topleft = (x,y)
+        self.rect = self.image.get_rect(topleft = pos)
+        self.c_rect = self.rect
         # player 이미지 크기
         # self.image = pygame.transform.scale(self.image, (50, 60))
         self.sx, self.sy = self.image.get_size()
         
         # player 위치
-        self.x = x
-        self.y = y
+        # self.x = pos[0]
+        # self.y = pos[1]
         
         # default
         self.move = 5   # 아이템에 따라 달라짐
         self.is_animating = False
         self.speed = 0.2   # 아이템에 따라 달라짐
+        self.obstacle_sprites = obstacle_sprites
+        self.direction = pygame.math.Vector2()
 
 	# 해당 player의 이미지를 screen의 어느 위치에 띄울지 정해주는 함수
-    def show(self):
-        self.screen.blit(self.image, (self.x,self.y))
+    # def show(self):
+    #     self.screen.blit(self.image, (self.x,self.y))
 
     # 키보드 입력값에 따라 player의 위치를 바꿔주는 함수
     def change_location(self, left_go,right_go,up_go,down_go):
         if left_go == True:
-            self.x -= self.move
+            self.direction.x = -1
+            #self.x -= self.move
             if self.current_sprite < 8:
                 self.current_sprite = 8
             self.current_sprite+=self.speed    # 속도 조절하는 ㅅㄲ
@@ -64,11 +65,14 @@ class player(pygame.sprite.Sprite):
                 self.current_sprite=8
                 self.is_animating = False   # 이거 설정하면, 어떤 입력값이 들어왔을 때 애니메이션 동작을 한번만 수행. 이거 설정 안하면 애니메이션 동작은 영구적 수행.
             self.image = self.sprites[int(self.current_sprite)]
+            if self.rect.x <= 0:
+                # self.x = 0
+                self.direction.x = 0
 
-            if self.x <= 0:
-                self.x = 0
+
         elif right_go == True:
-            self.x += self.move
+            self.direction.x = 1
+            #self.x += self.move
             if self.current_sprite < 12:
                 self.current_sprite = 12
             self.current_sprite+=self.speed    # 속도 조절하는 ㅅㄲ
@@ -76,11 +80,13 @@ class player(pygame.sprite.Sprite):
                 self.current_sprite=12
                 self.is_animating = False   # 이거 설정하면, 어떤 입력값이 들어왔을 때 애니메이션 동작을 한번만 수행. 이거 설정 안하면 애니메이션 동작은 영구적 수행.
             self.image = self.sprites[int(self.current_sprite)]
-            
-            if self.x >= size[0] - self.sx:
-                self.x = size[0] - self.sx
+            if self.rect.x >= size[0] - self.sx:
+                # self.x = size[0] - self.sx
+                self.direction.x = 0
+
         elif up_go == True:
-            self.y -= self.move
+            self.direction.y = -1
+            # self.y -= self.move
             if self.current_sprite < 4:
                 self.current_sprite = 4
             self.current_sprite+=self.speed    # 속도 조절하는 ㅅㄲ
@@ -88,21 +94,61 @@ class player(pygame.sprite.Sprite):
                 self.current_sprite=4
                 self.is_animating = False   # 이거 설정하면, 어떤 입력값이 들어왔을 때 애니메이션 동작을 한번만 수행. 이거 설정 안하면 애니메이션 동작은 영구적 수행.
             self.image = self.sprites[int(self.current_sprite)]
-            
-            if self.y <= 0:
-                self.y = 0
+            if self.rect.y <= 0:
+                #self.y = 0
+                self.direction.y=0
+
         elif down_go == True:
-            self.y += self.move
+            self.direction.y = 1
+            #self.y += self.move
             if self.current_sprite < 0:
                 self.current_sprite = 0
             self.current_sprite+=self.speed    # 속도 조절하는 ㅅㄲ
             if self.current_sprite>3:
                 self.current_sprite=0
                 self.is_animating = False   # 이거 설정하면, 어떤 입력값이 들어왔을 때 애니메이션 동작을 한번만 수행. 이거 설정 안하면 애니메이션 동작은 영구적 수행.
-            self.image = self.sprites[int(self.current_sprite)]
-                
-            if self.y >= size[1] - self.sy:
-                self.y = size[1] - self.sy
-                
-        self.rect = self.image.get_rect()
-        self.rect.topleft = [self.x,self.y]
+            self.image = self.sprites[int(self.current_sprite)]    
+            if self.rect.y >= size[1] - self.sy:
+                #self.y = size[1] - self.sy
+                self.direction.y=0
+        
+
+        
+        self.rect.x += self.direction.x * 5
+        self.c_rect = pygame.Rect(self.rect.x+15, self.rect.y+30, 50, 50)
+        print(self.rect, self.c_rect)
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * 5
+        self.c_rect = pygame.Rect(self.rect.x+15, self.rect.y+30, 50, 50)
+        self.collision('vertical')
+        self.direction.x=0
+        self.direction.y=0
+        
+        #print( )
+        #self.rect = self.image.get_rect()
+        self.rect.topleft = [self.rect.x,self.rect.y]
+        
+   
+    
+
+    def collision(self,direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.c_rect):
+                    #print(sprite.rect.colliderect(self.rect))
+                    #print(self.rect.x,self.rect.y)
+                    if self.direction.x > 0: # moving right
+                        #print("변하기전",self.rect.x,self.rect.y)
+                        self.rect.right = sprite.rect.left+15
+                        #print(self.rect.x,self.rect.y)
+
+                    if self.direction.x < 0: # moving left
+                        self.rect.left = sprite.rect.right-15
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.c_rect):
+                    if self.direction.y > 0: # moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: # moving up
+                        self.rect.top = sprite.rect.bottom-30
